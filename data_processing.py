6d98d5d2b7cdc9a383ecec722d56d28351caea74
tigran-serobyan <38076722+tigran-serobyan@users.noreply.github.com>
2022-12-12 16:57:33 +0400

    Create data_processing.py

---
+++ data_processing.py
@@ -0,0 +1,11 @@
+# Get the audiofile and sampling rate
+# Create a spectrogram
+# Return the spectrogram as a matrix
+def spectrogram(track, sr):
+
+
+# Other
+
+# Create a function to find when the word starts and ends
+# Create a function to check if the audiofile is empty or not
+# Create a function to check if the word is fully recorded or not
