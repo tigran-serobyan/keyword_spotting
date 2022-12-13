beeba9f2ea3b28e5fd6fc50e7733f6517f78acdb
tigran-serobyan <38076722+tigran-serobyan@users.noreply.github.com>
2022-12-13 10:56:38 +0400

    Update data_processing.py

--- data_processing.py
+++ data_processing.py
@@ -1,11 +1,4 @@
-# Get the audiofile and sampling rate
-# Create a spectrogram
-# Return the spectrogram as a matrix
-def spectrogram(track, sr):
+#import corresponding functions from other files
 
-
-# Other
-
-# Create a function to find when the word starts and ends
-# Create a function to check if the audiofile is empty or not
-# Create a function to check if the word is fully recorded or not
+def process(data):
+  pass
